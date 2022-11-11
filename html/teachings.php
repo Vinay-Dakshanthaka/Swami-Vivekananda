4e057108c4bdaa97ed9c5011d26022b508bbfd18
Vinay <vinayd098@gmail.com>
2022-11-11 21:12:25 +0530

    Second commit added teachings.php and some styles to set paragraph in all file

--- html/teachings.php
+++ html/teachings.php
@@ -1,10 +1,49 @@
 <?php
     include "../includes/header.php"
 ?>
+<div id="teaching">
+    <br>
+    <br>
+    <br>
+    <h1>TEACHINGS</h1>
+<p id="p-first">
+While synthesizing and popularizing various strands of Hindu-thought, most notably classical yoga and (Advaita) Vedanta, Vivekananda was influenced by western ideas such as Universalism, via Unitarian missionaries who collaborated with the Brahmo Samaj. His initial beliefs were shaped by Brahmo concepts, which included belief in a formless God and the deprecation of idolatry, and a "streamlined, rationalized, monotheistic theology strongly coloured by a selective and modernistic reading of the Upanisads and of the Vedanta". He propagated the idea that "the divine, the absolute, exists within all human beings regardless of social status", and that "seeing the divine as the essence of others will promote love and social harmony". Via his affiliations with Keshub Chandra Sen's Nava Vidhan, the Freemasonry lodge, the Sadharan Brahmo Samaj, and Sen's Band of Hope, Vivekananda became acquainted with Western esotericism.
+</p>
+<img src="../images/ramakrishna.jpg" alt="Ramakrishna Paramahamsa" id="ramakrishna" />
+<p>
+He was also influenced by Ramakrishna, who gradually brought Narendra to a Vedanta-based worldview that "provides the ontological basis for 'śivajñāne jīver sevā', the spiritual practice of serving human beings as actual manifestations of God.
+</p>
+
+<p>
+Vivekananda propagated that the essence of Hinduism was best expressed in Adi Shankara's Advaita Vedanta philosophy. Nevertheless, following Ramakrishna, and in contrast to Advaita Vedanta, Vivekananda believed that the Absolute is both immanent and transcendent.[no According to Anil Sooklal, Vivekananda's neo-Vedanta "reconciles Dvaita or dualism and Advaita or non-dualism," viewing Brahman as "one without a second," yet "both qualified, saguna, and qualityless, nirguna."[no Vivekananda summarised the Vedanta as follows, giving it a modern and Universalistic interpretation, showing the influence of classical yoga:
+</p>
+
+<blockquote>
+    &ldquo;
+Each soul is potentially divine. The goal is to manifest this Divinity within by controlling nature, external and internal. Do this either by work, or worship, or mental discipline, or philosophy—by one, or more, or all of these—and be free. This is the whole of religion. Doctrines, or dogmas, or rituals, or books, or temples, or forms, are but secondary details. &rdquo;
+</blockquote>
 
 
+<p>
+Vivekananda's emphasis on nirvikalpa samadhi was preceded by medieval yogic influences on Advaita Vedanta. In line with Advaita Vedanta texts like Dŗg-Dŗśya-Viveka (14th century) and Vedantasara (of Sadananda) (15th century), Vivekananda saw samadhi as a means to attain liberation.
+</p>
 
+<p>
+Vivekananda popularized the notion of involution, a term which Vivekananda probably took from western Theosophists, notably Helena Blavatsky, in addition to Darwin's notion of evolution, and possibly referring to the Samkhya term sātkarya. Theosophic ideas on involution has "much in common" with "theories of the descent of God in Gnosticism, Kabbalah, and other esoteric schools." According to Meera Nanda, "Vivekananda uses the word involution exactly how it appears in Theosophy: the descent, or the involvement, of divine cosnciousness into matter." With spirit, Vivekananda refers to prana or purusha, derived ("with some original twists") from Samkhya and classical yoga as presented by Patanjali in the Yoga sutras.
+</p>
 
+<p>
+Vivekananda linked morality with control of the mind, seeing truth, purity and unselfishness as traits which strengthened it. He advised his followers to be holy, unselfish and to have shraddhā (faith). Vivekananda supported brahmacharya, believing it the source of his physical and mental stamina and eloquence
+</p>
+
+<p>
+Vivekananda's acquaintance with Western esotericism made him very successful in Western esoteric circles, beginning with his speech in 1893 at the Parliament of Religions. Vivekananda adapted traditional Hindu ideas and religiosity to suit the needs and understandings of his Western audiences, who were especially attracted by and familiar with Western esoteric traditions and movements like Transcendentalism and New thought. An important element in his adaptation of Hindu religiosity was the introduction of his four yoga's model, which includes Raja yoga, his interpretation of Patanjali's Yoga sutras, which offered a practical means to realize the divine force within which is central to modern Western esotericism. In 1896, his book Raja Yoga was published, which became an instant success and was highly influential in the Western understanding of yoga.
+</p>
+
+<p>
+Nationalism was a prominent theme in Vivekananda's thought. He believed that a country's future depends on its people, and his teachings focused on human development. He wanted "to set in motion a machinery which will bring noblest ideas to the doorstep of even the poorest and the meanest"
+</p>
+</div>
 <?php
     include "../includes/footer.php"
 
